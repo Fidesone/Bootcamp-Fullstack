c78ada9965236ca1c59c8f8f5b5491870f97ff38
Héctor González Pereda <hgonza02@ucm.es>
2021-07-27 13:23:15 +0200

    Ejercicios 1.1 y 1.2: Introdución a React

--- part1/react_intro/src/index.js
+++ part1/react_intro/src/index.js
@@ -2,27 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
+  const Header_course= 'Ejercicio 1.1 & 1.2'
+  const Content = (props) => {
+    return (
+      <div>
+        <p>{props.name}</p>
+        <p>{props.name}</p>
+        <p>{props.name}</p>
+        
+      </div>
+    )
+  }
+  const Total = 10
+  
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+     <h1>{Header_course} </h1> 
+      <Content name= 'Parte 1'/>
+      <p>{Total} </p>
+      
+
     </div>
   )
 }
